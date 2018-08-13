a170044272300763340055e660bb0f2f350cd752
ILikePythonAndDjango <nik1995osipov@gmail.com>
2018-08-13 23:26:54 +0300

    new dependecies

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+npm install --save dy-parser@1.17.2
+npm install --save config@1.26.2
+npm install --save express@4.15.3
+npm install --save jsonwebtoken@7.4.2
+npm install --save mongoose@4.11.5
+npm install --save mongoose-paginate@5.0.3
+npm install --save morgan@1.8.2
+npm install --save pow-mongodb-fixtures@0.14.0
+
