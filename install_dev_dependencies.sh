b88619803bbc1d27ea1190390894d98be401ab17
ILikePythonAndDjango <nik1995osipov@gmail.com>
2018-08-13 22:22:19 +0300

    append script for installing development dependencies

---
+++ install_dev_dependencies.sh
@@ -0,0 +1,10 @@
+npm install --save-dev babel-cli@6.26.0
+npm install --save-dev babel-core@6.26.0
+npm install --save-dev babel-preset-es2015@6.24.1
+npm install --save-dev babel-preset-stage-0@6.24.1
+npm install --save-dev chai@4.1.2
+npm install --save-dev chai-http@3.0.0
+npm install --save-dev mocha@3.5.3
+npm install --save-dev nodemon@1.11.0
+npm install --save-dev rimraf@2.6.2
+
